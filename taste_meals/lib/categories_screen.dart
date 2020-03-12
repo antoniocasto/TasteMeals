72ad6e15ba2f8ae6f1159ea8241ec8db990468bc
antoniocasto <antonio9_4@outlook.com>
2020-03-12 16:26:01 +0100

    end of lesson 5 - Registering a screen as the main screen

--- taste_meals/lib/categories_screen.dart
+++ taste_meals/lib/categories_screen.dart
@@ -6,17 +6,22 @@ import 'dummy_data.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: DUMMY_CATEGORIES
-          .map((catData) =>
-              CategoryItem(title: catData.title, color: catData.color))
-          .toList(),
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent:
-            200, //dimensione di ogni item. La grid viene costruita in base alla dimensione dell'item
-        childAspectRatio: 3 / 2, //definisco le proporzioni dell'item
-        crossAxisSpacing: 20, //per spazio tra item
-        mainAxisSpacing: 20, //per spazio tra item
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('TasteMeals'),
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES
+            .map((catData) =>
+                CategoryItem(title: catData.title, color: catData.color))
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent:
+              200, //dimensione di ogni item. La grid viene costruita in base alla dimensione dell'item
+          childAspectRatio: 3 / 2, //definisco le proporzioni dell'item
+          crossAxisSpacing: 20, //per spazio tra item
+          mainAxisSpacing: 20, //per spazio tra item
+        ),
       ),
     );
   }
