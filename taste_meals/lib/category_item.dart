49bb82f62c5c97e23dbc8b503fcc8e20e148c167
antoniocasto <antonio9_4@outlook.com>
2020-03-12 13:05:29 +0100

    fix a ";"

--- taste_meals/lib/category_item.dart
+++ taste_meals/lib/category_item.dart
@@ -5,10 +5,10 @@ class CategoryItem extends StatelessWidget {
   final String title;
   final Color color;
 
-  CategoryItem({this.title, this.color})
+  CategoryItem({this.title, this.color});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return Container(
       padding: const EdgeInsets.all(15),
       child: Text(title),
