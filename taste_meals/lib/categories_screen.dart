37644a11cfcd2bc37c0a601e4baa18e0eb15637d
antoniocasto <antonio9_4@outlook.com>
2020-03-12 13:02:29 +0100

    end of lesson 4 - Dummy data, Category Itemm, GridView and Linear Gradient

---
+++ taste_meals/lib/categories_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:taste_meals/category_item.dart';
+
+import 'dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      children: DUMMY_CATEGORIES
+          .map((catData) =>
+              CategoryItem(title: catData.title, color: catData.color))
+          .toList(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent:
+            200, //dimensione di ogni item. La grid viene costruita in base alla dimensione dell'item
+        childAspectRatio: 3 / 2, //definisco le proporzioni dell'item
+        crossAxisSpacing: 20, //per spazio tra item
+        mainAxisSpacing: 20, //per spazio tra item
+      ),
+    );
+  }
+}
