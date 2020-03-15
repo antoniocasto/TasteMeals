95c5b499a3ea03bca5a0fcb6b4c81ac8442b2eb3
antoniocasto <antonio9_4@outlook.com>
2020-03-15 16:49:25 +0100

    end of lesson 22 - Adding a Custom Drawer

--- taste_meals/lib/screens/tabs_screen.dart
+++ taste_meals/lib/screens/tabs_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:taste_meals/screens/categories_screen.dart';
 import 'package:taste_meals/screens/favourites_screen.dart';
+import 'package:taste_meals/widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   @override
@@ -27,6 +28,7 @@ class _TabsScreenState extends State<TabsScreen> {
       appBar: AppBar(
         title: Text(_pages[_selectedPageIndex]['title']),
       ),
+      drawer: MainDrawer(),
       body: SafeArea(
         child: _pages[_selectedPageIndex]['page'],
       ),
