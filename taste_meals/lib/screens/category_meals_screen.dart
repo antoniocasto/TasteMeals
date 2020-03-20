ce23fb7597395e1751e2f3cfbe7ca32e8ba854ca
antoniocasto <antonio9_4@outlook.com>
2020-03-20 13:14:07 +0100

    end of lesson 26 - Popping Pages & Passing Data Back

--- taste_meals/lib/screens/category_meals_screen.dart
+++ taste_meals/lib/screens/category_meals_screen.dart
@@ -1,26 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:taste_meals/models/meal.dart';
 import 'package:taste_meals/widgets/meal_item.dart';
 
 import '../dummy_data.dart';
 
-class CategoryMealsScreen extends StatelessWidget {
+class CategoryMealsScreen extends StatefulWidget {
   static const routeName = '/category-meals';
 
-  //Qui sotto usati senza le route con una sola pagina
-  // final String categoryId;
-  // final String categoryTitle;
-  // CategoryMealsScreen({this.categoryId, this.categoryTitle});
+  @override
+  _CategoryMealsScreenState createState() => _CategoryMealsScreenState();
+}
+
+class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
+  String categoryTitle;
+  List<Meal> displayedMeals;
+  bool _loadedInitData = false;
+
+  @override
+  void initState() {
+    //...
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    //no need to call setstate here
+    if (_loadedInitData == false) {
+      final routeArgs =
+          ModalRoute.of(context).settings.arguments as Map<String, String>;
+      categoryTitle = routeArgs['title'];
+      final categoryId = routeArgs['id'];
+      displayedMeals = DUMMY_MEALS.where((meal) {
+        return meal.categories.contains(categoryId);
+      }).toList();
+      _loadedInitData = true;
+    }
+
+    super.didChangeDependencies();
+  }
+
+  void _removeMeal(String mealId) {
+    setState(() {
+      displayedMeals.removeWhere((item) => item.id == mealId);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     //Estraggo arguments del pushNamed
-    final routeArgs =
-        ModalRoute.of(context).settings.arguments as Map<String, String>;
-    final categoryTitle = routeArgs['title'];
-    final categoryId = routeArgs['id'];
-    final categoryMeals = DUMMY_MEALS.where((meal) {
-      return meal.categories.contains(categoryId);
-    }).toList();
 
     return Scaffold(
       appBar: AppBar(
@@ -28,15 +55,16 @@ class CategoryMealsScreen extends StatelessWidget {
       ),
       body: SafeArea(
         child: ListView.builder(
-          itemCount: categoryMeals.length,
+          itemCount: displayedMeals.length,
           itemBuilder: (ctx, index) {
             return MealItem(
-              id: categoryMeals[index].id,
-              title: categoryMeals[index].title,
-              imageUrl: categoryMeals[index].imageUrl,
-              duration: categoryMeals[index].duration,
-              complexity: categoryMeals[index].complexity,
-              affordability: categoryMeals[index].affordability,
+              id: displayedMeals[index].id,
+              title: displayedMeals[index].title,
+              imageUrl: displayedMeals[index].imageUrl,
+              duration: displayedMeals[index].duration,
+              complexity: displayedMeals[index].complexity,
+              affordability: displayedMeals[index].affordability,
+              removeItem: _removeMeal,
             );
           },
         ),
